1582d99a4242be2e30288f070dfe779284ef7906
Angusbyte <87798412+Angusbyte@users.noreply.github.com>
2021-12-14 13:57:36 +1100

    Test 1
    Testing #1

--- finance/finance/urls.py
+++ finance/finance/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('Home page')
+
+def room(request):
+    return HttpResponse('ROOM')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room)
 ]
